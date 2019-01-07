8fa325f9e5ea6d61db922bbe0c85fd3c5a726708
lvyonghao <37901730+snake-lvyonghao@users.noreply.github.com>
2019-01-08 01:02:29 +0800

    添加排序算法

--- data_struct_source/Sort/HeapSort.c
+++ data_struct_source/Sort/HeapSort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
-	@param arr 待调整的数组
+	@param sort 待调整的数组
 	@param i 待调整的结点的下标
 	@param length 数组的长度
 */
-void HeapAdjust(int arr[], int i, int length)
+void HeapAdjust(int sort[], int i, int length)
 {
     // 调整i位置的结点
     // 先保存当前结点的下标
@@ -12,11 +12,11 @@ void HeapAdjust(int arr[], int i, int length)
     // 当前结点左右孩子结点的下标
     int lchild = i * 2 + 1;
     int rchild = i * 2 + 2;
-    if (lchild < length && arr[lchild] > arr[max])
+    if (lchild < length && sort[lchild] > sort[max])
     {
         max = lchild;
     }
-    if (rchild < length && arr[rchild] > arr[max])
+    if (rchild < length && sort[rchild] > sort[max])
     {
         max = rchild;
     }
@@ -24,31 +24,31 @@ void HeapAdjust(int arr[], int i, int length)
     if (max != i)
     {
         int temp;
-        temp = arr[i];
-        arr[i] = arr[max];
-        arr[max] = temp;
+        temp = sort[i];
+        sort[i] = sort[max];
+        sort[max] = temp;
         // 递归
-        HeapAdjust(arr, max, length);
+        HeapAdjust(sort, max, length);
     }
 }
 
 // 堆排序
-void HeapSort(int arr[], int length)
+void HeapSort(int sort[], int length)
 {
     // 初始化堆
     // length / 2 - 1是二叉树中最后一个非叶子结点的序号
     for (int i = length / 2 - 1; i >= 0; i--)
     {
-        HeapAdjust(arr, i, length);
+        HeapAdjust(sort, i, length);
     }
     // 交换堆顶元素和最后一个元素
     for (int i = length - 1; i >= 0; i--)
     {
         int temp;
-        temp = arr[i];
-        arr[i] = arr[0];
-        arr[0] = temp;
-        HeapAdjust(arr, 0, i);
+        temp = sort[i];
+        sort[i] = sort[0];
+        sort[0] = temp;
+        HeapAdjust(sort, 0, i);
     }
 }
 
